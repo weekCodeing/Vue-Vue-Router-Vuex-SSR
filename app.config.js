85c536327a9be5d5f7de05f5cc74054af0b66060
webVueBlog <2979986590@qq.com>
2022-02-19 22:12:53 +0800

    add more

---
+++ app.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    db: {
+        appId: 'x',
+        appKey: 'x'
+    },
+    cdn: {
+        host: 'hx',
+        bucket: 'vue-study',
+        ak: 'x',
+        sk: 'x'
+    }
+}
